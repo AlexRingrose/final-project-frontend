4d92837eea757ee24d29d547e50ff68d296946b1
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-11-20 13:10:52 -0800

    a

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,16 +5,19 @@ import { HttpClient } from '@angular/common/http'
   providedIn: 'root'
 })
 export class ApiService {
-  // apiKey;
-  // apiBasePath;
-  // apiFunction;
+  apiKey;
+  apiBasePath;
+  apiFunction;
+  apiInterval;
   constructor(public _http : HttpClient) {
-    // this.apiKey = "O5PZCEDH04Z1PEWN";
-    // this.apiBasePath = "https://www.alphavantage.co/query?"
-    // this.apiFunction = "TIME_SERIES_INTRADAY"
+    this.apiKey = "O5PZCEDH04Z1PEWN";
+    this.apiBasePath = "https://www.alphavantage.co/query?";
+    this.apiFunction = "TIME_SERIES_INTRADAY";
+    this.apiInterval = "5min";
   }
-  
+
   searchStock(ticker){
-    return this._http.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval=5min&apikey=demo");
+    return this._http.get(this.apiBasePath+"function="+this.apiFunction+
+      "&symbol="+ticker+"&interval="+this.apiInterval+"&apikey="+this.apiKey);
   }
 }
