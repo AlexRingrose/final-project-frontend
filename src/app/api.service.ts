b28ae349cee5d933be7f8bd37ab064203f27ce23
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-11-20 20:37:29 +0000

    sample chart

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  // apiKey;
+  // apiBasePath;
+  // apiFunction;
+  constructor(public _http : HttpClient) {
+    // this.apiKey = "O5PZCEDH04Z1PEWN";
+    // this.apiBasePath = "https://www.alphavantage.co/query?"
+    // this.apiFunction = "TIME_SERIES_INTRADAY"
+  }
+  
+  searchStock(ticker){
+    return this._http.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval=5min&apikey=demo");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
