917b6afd1a66ea6b10f5e2c7d9ef69f404212684
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-12-01 15:36:08 -0800

    Linking up chart

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,12 +12,11 @@ export class ApiService {
   constructor(public _http : HttpClient) {
     this.apiKey = "O5PZCEDH04Z1PEWN";
     this.apiBasePath = "https://www.alphavantage.co/query?";
-    this.apiFunction = "TIME_SERIES_INTRADAY";
     this.apiInterval = "5min";
   }
 
-  searchStock(ticker){
-    return this._http.get(this.apiBasePath+"function="+this.apiFunction+
-      "&symbol="+ticker+"&interval="+this.apiInterval+"&apikey="+this.apiKey);
+  searchStock(ticker,apiFunction){
+    return this._http.get(this.apiBasePath+"function="+apiFunction+
+      "&symbol="+ticker+"&interval="+this.apiInterval+"&apikey="+this.apiKey+"&outputsize=compact");
   }
 }
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -8,16 +8,18 @@ import { ApiService } from '../api.service';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
-
+  dataSet:Array;
   constructor(public _api:ApiService) { }
 
   ngOnInit() {
   }
 
   myClick(){
-    this._api.searchStock("MSFT").subscribe(
+    this._api.searchStock("MSFT","TIME_SERIES_INTRADAY").subscribe(
       (res) => {
-        console.log(res)
+        console.log("Api Response",res)
+        console.log("time series",res["Time Series (5min)"])
+        console.log("data point",res["Time Series (5min)"][] )//["2018-11-30 16:00:00"])
       }
     );
   }
@@ -25,11 +27,9 @@ export class ChartComponent implements OnInit {
   // lineChart
     //
     public lineChartData:Array<any> = [
-      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-      {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-      {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
     ];
-    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartLabels:Array<any> = ['11', '12', '1', '2', '3', '4', '5'];
     public lineChartOptions:any = {
       responsive: true
     };
@@ -61,24 +61,4 @@ export class ChartComponent implements OnInit {
     ];
     public lineChartLegend:boolean = true;
     public lineChartType:string = 'line';
-
-    public randomize():void {
-      let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-      for (let i = 0; i < this.lineChartData.length; i++) {
-        _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-          _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-        }
-      }
-      this.lineChartData = _lineChartData;
-    }
-
-    // events
-    public chartClicked(e:any):void {
-      console.log(e);
-    }
-
-    public chartHovered(e:any):void {
-      console.log(e);
-    }
 }
