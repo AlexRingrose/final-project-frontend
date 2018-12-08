ec585d2acf122f81f76fc05489b6dd8cae4c8f9e
AlexRingrose <ringrose.alex@gmail.com>
2018-12-07 20:57:12 -0800

    Multi search working

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -9,9 +9,8 @@ import { ApiService } from '../api.service';
 })
 export class ChartComponent implements OnInit {
   public lineChartData: Array<any> =
-      [{data: [], label: '' },
-      { data: [], label: '' },
-      { data: [], label: '' }];
+      [{data: [ 0, 0, 0, 0, 0, 0, 0 ], label: '1' },
+      { data: [ 0, 0, 0, 0, 0, 0, 0 ], label: '2' }];
   public lineChartLabels: Array<any> =
       ['1', '2', '3', '4', '5', '6', '7'];
   public lineChartOptions: any = {
@@ -46,49 +45,47 @@ export class ChartComponent implements OnInit {
   public lineChartLegend = true;
   public lineChartType = 'line';
 
-  private search = [ '', '', ''];
-  private dataSet = [];
+  private search = [ '', ''];
+  private dataSet = [ { data: [ 0, 0, 0, 0, 0, 0, 0 ], label: '1' },
+  { data: [ 0, 0, 0, 0, 0, 0, 0 ], label: '2' }];
 
   constructor(public _api: ApiService) {}
 
   validInput(input) {
-    if ( input !== '') {
+    if ( input !== '' && input.length > 3) {
       return true;
     }
   }
 
-  updateChart () {
-    for (let i = 0; i < this.search.length; i++) {
-      if ( this.validInput(this.search[i].toUpperCase() )) {
-        this.dataSet[ i ] = this.getStockData( this.search[ i ] );
-      } else {
-        this.dataSet[ i ] = { data: [], label: '' };
+  getStockData() {
+
+    for ( let i = 0; i < this.search.length; i++ ) {
+      if ( this.validInput( this.search[ i ] ) ) {
+        this._api.stockDaily( this.search[ i ] ).subscribe(
+          ( res ) => {
+            const dataAry = [];
+
+            console.log( 'Api Response', res );
+
+            for ( const data of Object.values( res[ 'Time Series (Daily)' ] ) ) {
+              dataAry.push( data[ '4. close' ] );
+            }
+
+            this.dataSet[i] = {
+              data: dataAry, label: this.search[ i ].toUpperCase() };
+            this.lineChartData = this.dataSet.slice(0);
+          }
+        );
       }
-      this.lineChartData[i] = this.dataSet [ i ];
     }
-  }
-
-  getStockData(ticker) {
-    this._api.stockDaily( ticker ).subscribe(
-      ( res ) => {
-        const dataAry = [];
-
-        console.log( 'Api Response', res );
-
-        for ( const data of Object.values( res[ 'Time Series (Daily)' ] ) ) {
-          dataAry.push( data[ '4. close' ] );
-        }
-
-        return { data: dataAry, label: ticker };
-      }
-    );
   }
 
   loadDummy() {
     const dataAry = [ 104.8200, 109.1900, 108.5200, 112.0900,
       110.8900, 110.1900, 111.1200 ];
 
-    this.lineChartData = [ { data: dataAry, label: 'MSFT' }];
+    this.lineChartData = [ { data: dataAry, label: 'MSFT' },
+    { data: [ 0, 0, 0, 0, 0, 0, 0 ], label: '2' }];
   }
 
   ngOnInit () {
