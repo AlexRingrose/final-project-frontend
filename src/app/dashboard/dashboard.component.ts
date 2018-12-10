c122d93ac43d27478788a452dfc6ef6cdfe6f3da
AlexRingrose <ringrose.alex@gmail.com>
2018-12-09 17:39:20 -0800

    Rearranging Header

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component} from '@angular/core';
 import { ApiService } from '../api.service';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+
 
 import { ChartComponent } from '../chart/chart.component';
 import { HeaderComponent } from '../header/header.component';
@@ -12,9 +14,11 @@ import { HeaderComponent } from '../header/header.component';
 })
 export class DashboardComponent {
   viewControl;
-  constructor(public _api: ApiService, private router: Router) {
+  constructor(public _api: ApiService, private _userServ: UserService, private router: Router) {
     this.viewControl = 'home';
   }
-
+  logOut () {
+    this._userServ.logOut();
+  }
 
 }
