a30d4467e5abda1849d1930effeb85f6efdb595a
AlexRingrose <ringrose.alex@gmail.com>
2018-12-12 19:12:59 -0800

    Home favorites and search working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
@@ -7,18 +7,18 @@ import { UserService } from '../user.service';
 import { ChartComponent } from '../chart/chart.component';
 import { HeaderComponent } from '../header/header.component';
 
-@Component({
+@Component( {
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
-})
+  styleUrls: [ './dashboard.component.css' ]
+} )
 export class DashboardComponent {
   viewControl;
-  constructor(public _api: ApiService, private _userServ: UserService, private router: Router) {
-    this.viewControl = 'home';
+  constructor ( public _api: ApiService, private _userServ: UserService, private router: Router ) {
+    this._api.viewControl = 'home';
   }
 
-  switchView(view) { this.viewControl = view; }
+  switchView ( view ) { this._api.viewControl = view; }
 
   logOut () { this._userServ.logOut(); }
 
